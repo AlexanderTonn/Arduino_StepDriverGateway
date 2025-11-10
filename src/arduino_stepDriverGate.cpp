74f3ee0528a649dfd1d362b268fa4f984e0dc53d
Alexander Tonn <alexandertonn@gmx.de>
2025-11-10 17:47:08 +0100

    Initial commit of implementation

---
+++ src/arduino_stepDriverGate.cpp
@@ -0,0 +1,176 @@
+#include "arduino_stepDriverGate.hpp"
+
+auto Arduino_StepDriverGate::run() -> void
+{
+    mPerfomanceStart = micros();
+    static auto init = false;
+    if (!init)
+    {
+        pinMode(mAnalogPin, INPUT);
+        pinMode(mStepPin, OUTPUT);
+        pinMode(mDirPin, OUTPUT);
+        pinMode(mEnablePin, OUTPUT);
+        init = true;
+    }
+
+    // calculate target step from input voltage
+    auto raw = analogRead(mAnalogPin);
+    mVoltage = fmap(raw, 0.0f, 1023.0f, 0.0f, 5000.0f);
+    mVoltageStepResolution = (mAnalogMax - mAnalogMin) / mMaxSteps;
+    mTargetStep = static_cast<uint16_t>((mVoltage - mAnalogMin) / mVoltageStepResolution);
+
+    if (mVoltage >= mAnalogMin && mVoltage <= mAnalogMax)
+    {
+        handle(mTargetStep);
+    }else 
+    {
+        handle(0);
+    }
+    mPerformanceEnd = micros();
+}
+
+/**
+ * @brief public method to set the stepper motor frequency for the step pulse (Hz)
+ * 
+ * @param frequency - Hz - see datasheet of stepper motor
+ */
+auto Arduino_StepDriverGate::setFrequency(uint16_t frequency) -> void
+{
+    if (frequency < 1)
+        mFrequency = 1;
+
+    mFrequency = frequency;
+}
+
+
+/**
+ * @brief create a step pulse for the stepper motor
+ *
+ */
+auto Arduino_StepDriverGate::handle(const uint16_t _targetStep) -> void
+{
+
+    if (mCurrentStep < _targetStep)
+    {
+        createSignal(direction::NORMAL);
+    }
+    else if ( (mCurrentStep > _targetStep) )
+    {
+        createSignal(direction::INVERTED);
+    }
+
+    setOutputs();
+
+}
+auto Arduino_StepDriverGate::createSignal(direction _dir) -> void
+{
+    auto timestamp = micros();
+    static auto timestampMicrosStepPulse = timestamp;
+    static auto timestampMicrosHigh = timestamp;
+    static auto timestampMicrosLow = timestamp;
+    auto period = 1000000 / mFrequency; // in microseconds
+
+    // Trigger step
+    mPauseWasActive = timestamp >= timestampMicrosLow + mLowTime;
+
+
+    // If drirection changed, set a pause for stabilization
+    bool directionNow = (_dir == direction::INVERTED) ? true : false;
+    if (mDirectionChanged != directionNow)
+    {
+        mPulseStepState = PulseStep::ACTIVE;
+        mDirectionChanged = directionNow;
+        timestampMicrosLow = timestamp;
+        timestampMicrosStepPulse = timestamp;
+        timestampMicrosHigh = timestamp;
+    }
+
+    switch (_dir)
+    {
+    case direction::NORMAL:
+        mOut.dir = false; // CW
+        break;
+    case direction::INVERTED:
+        mOut.dir = true; // CCW
+        break;
+    }
+
+    switch (mPulseStepState)
+    {
+    case PulseStep::ACTIVE:
+
+        mPulseSignalActive = timestamp >= timestampMicrosStepPulse + period;
+
+        if (mPulseSignalActive)
+        {
+            timestampMicrosStepPulse = timestamp;
+            timestampMicrosLow = timestamp;
+
+            mOut.Sig = true;
+            mPulseStepState = PulseStep::INACTIVE;
+        }
+        break;
+
+    case PulseStep::INACTIVE:
+
+        mPulseSignalInactive = timestamp >= timestampMicrosHigh + mHighTime;
+
+        if (mPulseSignalInactive)
+        {
+
+            timestampMicrosStepPulse = timestamp;
+            timestampMicrosHigh = timestamp;
+
+            mOut.Sig = false;
+
+            mPulseStepState = PulseStep::PAUSE;
+        }
+        break;
+    case PulseStep::PAUSE:
+        if (mPauseWasActive)
+        {
+            timestampMicrosLow = timestamp;
+            
+
+            switch (_dir)
+            {
+            case direction::NORMAL:
+                mCurrentStep++;
+                break;
+            case direction::INVERTED:
+                mCurrentStep--;
+                break;
+            }
+
+            mPulseStepState = PulseStep::ACTIVE;
+        }
+        break;
+    }
+}
+
+/**
+ * @brief Stops the stepper motor by disabling the driver immediately.
+ * @brief The stepper will stop moving and hold its current position.
+ */
+auto Arduino_StepDriverGate::stop() -> void
+{
+    // Disable Stepper Driver
+    mOut.en = false;
+    mOut.Sig = false;
+}
+
+auto Arduino_StepDriverGate::setOutputs() -> void
+{
+    digitalWrite(mStepPin, mOut.Sig);
+    digitalWrite(mDirPin, mOut.dir);
+    digitalWrite(mEnablePin, mOut.en);
+}
+/**
+ * @brief Function to get the performance time of the last run loop in microseconds
+ * @brief this is useful for checking the processing time in realtime applications
+ * @return uint32_t - time in microseconds
+ */
+auto Arduino_StepDriverGate::getPerformance() -> uint32_t 
+{
+    return mPerformanceEnd - mPerfomanceStart;
+}
---
+++ src/arduino_stepDriverGate.hpp
@@ -0,0 +1,84 @@
+#ifndef ARDUINO_STEPDRIVERGATE_HPP
+#define ARDUINO_STEPDRIVERGATE_HPP
+
+#include <Arduino.h>
+
+class Arduino_StepDriverGate 
+    {
+        
+    private:
+        byte mAnalogPin;
+        byte mStepPin;
+        byte mDirPin;
+        byte mEnablePin;
+        uint16_t mMaxSteps = 200; // Steps per Revolution
+
+        uint16_t mCurrentStep = 0; // Current Step Position
+        uint16_t mTargetStep = 0; // Target Step Position
+        uint16_t mFrequency = 50; //Hz
+        uint16_t mHighTime = 20; //micro seconds
+        uint16_t mLowTime = 20; //micro seconds
+        float mVoltage = 0.0f;
+        uint16_t mVoltageStepResolution = 0; // Steps derived from Voltage
+
+        float mAnalogMin = 500.0; // Min Value in mV
+        float mAnalogMax = 5000.0; // Max Value  in mV
+
+        bool mPulseSignalActive = false; 
+        bool mPulseSignalInactive = false;
+        bool mPauseWasActive = false;
+        bool mDirectionChanged = false; // used for direction change detection
+        bool mPulseInverted = false;
+
+        uint32_t mPerfomanceStart;
+        uint32_t mPerformanceEnd;
+
+        struct outputs 
+        {
+            bool Sig;
+            bool en;
+            bool dir;
+        } mOut;
+
+
+    public:
+        Arduino_StepDriverGate(byte analogPin, byte stepPin, byte dirPin, byte enPin, uint16_t maxSteps) 
+        : mAnalogPin(analogPin), mStepPin(stepPin), mDirPin(dirPin), mEnablePin(enPin), mMaxSteps(maxSteps) {}
+
+        enum class direction
+        {
+            NORMAL,
+            INVERTED
+        };
+        enum class PulseStep
+        {
+            ACTIVE,
+            INACTIVE,
+            PAUSE
+
+        } mPulseStepState;
+
+        auto setFrequency(const uint16_t frequency) -> void;
+        auto setAnalogMin(const uint16_t analogMin) -> void {mAnalogMin = analogMin;};
+        auto setAnalogMax(const uint16_t analogMax) -> void {mAnalogMax = analogMax;};
+        auto setEnable(const bool enable) -> void {mOut.en = enable;};
+        auto setInvertPulse(const bool invertPulse) -> void {mPulseInverted = invertPulse;};
+        auto setHighTime(const uint16_t highTime) -> void {mHighTime = highTime;};
+        auto setLowTime(const uint16_t lowTime) -> void {mLowTime = lowTime;};
+        auto getPerformance() -> uint32_t ;
+
+        auto run() -> void;
+        auto stop() -> void;
+
+        private:
+        auto handle(const uint16_t _targetStep) -> void;
+        auto fmap(float x, float in_min, float in_max, float out_min, float out_max) -> float
+        {
+            return out_min + (x - in_min) * (out_max - out_min) / (in_max - in_min);
+        }
+        auto setOutputs() -> void;
+
+        auto createSignal(direction _dir) -> void;
+};
+
+#endif // ARDUINO_STEPDRIVERGATE_HPP
